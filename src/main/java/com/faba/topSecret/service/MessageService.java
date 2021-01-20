1618152f08c48e78194768cec00f8c5e497b1991
Fabrizio Fernandez <ffernandez@certant.com.ar>
2021-01-20 17:12:42 -0300

    messageService test

--- src/main/java/com/faba/topSecret/service/MessageService.java
+++ src/main/java/com/faba/topSecret/service/MessageService.java
@@ -2,6 +2,11 @@ package com.faba.topSecret.service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+
 @Service
 public class MessageService {
+    public String getMessage(ArrayList<String> array1, ArrayList<String> array2, ArrayList<String> array3) {
+        return "this is not implemented";
+    }
 }
