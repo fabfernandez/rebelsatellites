f3bc851987ad0f37f37a3f27528ce91193af9225
Fabrizio Fernandez <ffernandez@certant.com.ar>
2021-01-21 16:58:54 -0300

    Solution to getMessage and tests

--- src/test/java/com/faba/topSecret/MessageServiceTest.java
+++ src/test/java/com/faba/topSecret/MessageServiceTest.java
@@ -16,22 +16,97 @@ class MessageServiceTest {
     MessageService messageService;
 
     @Test
-    void getMessage() {
-        ArrayList<String> satellite1 = new ArrayList<>(
-                List.of("", "this", "is", "a", "secret", "message")
-        );
-        ArrayList<String> satellite2 = new ArrayList<>(
-                List.of("this", "", "a", "secret", "message")
-        );
-        ArrayList<String> satellite3 = new ArrayList<>(
-                List.of("", "", "is", "a", "", "message")
-        );
-
+    void decipherSimpleMessage() {
         String solution = "this is a secret message";
 
-        String message = messageService.getMessage(satellite1, satellite2, satellite3);
+        ArrayList<String> msg1 = new ArrayList<>(
+                List.of("",  //lag
+                        "this", "is", "a", "secret", "message")
+        );
+        ArrayList<String> msg2 = new ArrayList<>(
+                List.of("this", "", "a", "secret", "message")
+        );
+        ArrayList<String> msg3 = new ArrayList<>(
+                List.of("", //lag
+                        "", "is", "a", "", "message")
+        );
+
+        ArrayList<ArrayList<String>> msgList = new ArrayList<>(List.of(msg1, msg2, msg3));
+
+        String message = messageService.getMessage(msgList);
 
         Assertions.assertEquals(message, solution);
-
     }
+
+    @Test
+    void decipherLongMessage() {
+        String solution = "this is a much longer message hail the empire and the dark lord send more troopers";
+
+        ArrayList<String> msg1 = new ArrayList<>(
+                List.of("", //lag
+                        "", "is", "", "much", "", "message", "", "the", "", "and", "", "dark", "", "send", "", "troopers")
+        );
+        ArrayList<String> msg2 = new ArrayList<>(
+                List.of("this", "", "", "much", "", "", "hail", "", "", "and", "", "", "lord", "", "", "troopers")
+        );
+        ArrayList<String> msg3 = new ArrayList<>(
+                List.of("", "", //lag
+                        "", "", "a", "", "longer", "message", "", "the", "empire", "", "the", "dark", "lord", "", "more", "troopers")
+        );
+
+        ArrayList<ArrayList<String>> msgList = new ArrayList<>(List.of(msg1, msg2, msg3));
+
+        String message = messageService.getMessage(msgList);
+
+        Assertions.assertEquals(message, solution);
+    }
+
+    @Test
+    void decipherOneFullMsgAndTwoEmpty() {
+        String solution = "we need to go deeper";
+
+        ArrayList<String> msg1 = new ArrayList<>(
+                List.of("", "", "", "", //lag
+                        "", "", "", "", "")
+        );
+        ArrayList<String> msg2 = new ArrayList<>(
+                List.of("",  //lag
+                        "we", "need", "to", "go", "deeper")
+        );
+        ArrayList<String> msg3 = new ArrayList<>(
+                List.of("", "", //lag
+                        "", "", "", "", "")
+        );
+
+        ArrayList<ArrayList<String>> msgList = new ArrayList<>(List.of(msg1, msg2, msg3));
+
+        String message = messageService.getMessage(msgList);
+
+        Assertions.assertEquals(message, solution);
+    }
+
+    @Test
+    void decipherOneWordOnEachMsg() {
+        String solution = "send more troopers";
+
+        ArrayList<String> msg1 = new ArrayList<>(
+                List.of("", "", "", "", "", "",  //lag
+                        "", "", "troopers")
+        );
+        ArrayList<String> msg2 = new ArrayList<>(
+                List.of("", "", "", //lag
+                        "", "more", "")
+        );
+        ArrayList<String> msg3 = new ArrayList<>(
+                List.of("", "", "", "", //lag
+                        "send", "", "")
+        );
+
+        ArrayList<ArrayList<String>> msgList = new ArrayList<>(List.of(msg1, msg2, msg3));
+
+        String message = messageService.getMessage(msgList);
+
+        Assertions.assertEquals(message, solution);
+    }
+
 }
